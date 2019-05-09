c0d6022f80ef754ca183a0bb9e71128c8ef19ecd
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-09 10:20:55 -0700

    Fixed indenting

--- BobaFridays/src/FoodOrder.java
+++ BobaFridays/src/FoodOrder.java
@@ -1,11 +1,10 @@
 
 public class FoodOrder extends T4Order {
-			private String customerName;
-			private String order;
-			private double orderPrice;
+	private String customerName;
+	private String order;
+	private double orderPrice;
 
-			public FoodOrder(String name, String o, double price) {
-				super(name, o, price);
-			}	
-			
+	public FoodOrder(String name, String o, double price) {
+		super(name, o, price);
+	}
 }
