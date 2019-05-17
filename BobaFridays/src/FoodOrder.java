b305321c9f356bd2a7dca24f652b2483a0c24868
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-17 10:43:33 -0700

    got rid of redundant instance variables

--- BobaFridays/src/FoodOrder.java
+++ BobaFridays/src/FoodOrder.java
@@ -1,9 +1,6 @@
 
 public class FoodOrder extends T4Order {
-	private String customerName;
-	private String order;
-	private double orderPrice;
-
+	
 	public FoodOrder(String o, double price) {
 		super(o, price);
 	}
