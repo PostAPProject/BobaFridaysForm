5ce3ad82d571540cbaa176d2aec1129e418e213b
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-17 10:41:49 -0700

    Updated Constructor

--- BobaFridays/src/FoodOrder.java
+++ BobaFridays/src/FoodOrder.java
@@ -4,7 +4,7 @@ public class FoodOrder extends T4Order {
 	private String order;
 	private double orderPrice;
 
-	public FoodOrder(String name, String o, double price) {
-		super(name, o, price);
+	public FoodOrder(String o, double price) {
+		super(o, price);
 	}
 }
