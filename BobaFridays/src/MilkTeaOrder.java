78c6eaf532e57d8f49fcc7c55bef5d84a25143a2
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-19 14:57:51 -0700

    Cleaned up redundant instance variables 5/19/19

--- BobaFridays/src/MilkTeaOrder.java
+++ BobaFridays/src/MilkTeaOrder.java
@@ -1,20 +1,11 @@
 import java.util.ArrayList;
 
 public class TeaOrder extends DrinkOrder {
-		private String customerName;
-		private String order;
-		private ArrayList<String> toppings;
-		private double orderPrice;
-		private boolean isHot;
 
-		public TeaOrder(String name, String o) {
-			super(name, o, t, price);
-			this.isHot = isHot;
+		public TeaOrder(String o, ArrayList<String> t, double price, boolean isHot) {
+			super(o, t, price);
 			if(isHot) {
-				orderPrice += 0.50;
-			}
-			for(String topping : toppings) {
-				orderPrice += 0.50;
-			}
-			
+				price += 0.50;
+			}	
 		}
+}
