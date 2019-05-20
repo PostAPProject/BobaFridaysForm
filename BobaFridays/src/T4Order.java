0bd8fe517dc36fe5a768faa4b0282b00fc8d0825
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-19 22:32:03 -0700

    added round2 method from Calculate

--- BobaFridays/src/T4Order.java
+++ BobaFridays/src/T4Order.java
@@ -1,10 +1,9 @@
 /*
- * @author Daric Zhou
+ * @author Daric Zhou, Jakin Chan, Jon Lee
  * @version May 2019
  * This superclass stores an order of a drink or food
  */
 public class T4Order {
-	private String customerName;
 	private String order;
 	protected double orderPrice;
 	
@@ -12,18 +11,32 @@ public class T4Order {
 		order = o;
 		setOrderPrice(price);
 	}
-	
-	public String getCustomerName() {
-		return customerName;
-	}
 	public String getOrderName() {
 		return order;
 	}
 	public double getPrice() {
-		return orderPrice * 1.0875;
+		return round2(orderPrice * 1.0875);
 	}
 
 	public void setOrderPrice(double orderPrice) {
 		this.orderPrice = orderPrice;
 	}
+	
+	//Takes a double, returns it rounded to the hundreths place
+	public static double round2(double num) {
+		
+		if(num == 0) {
+			return 0;
+		}
+		
+		int sign = (int) (num / Math.abs(num));
+		int answer = Math.abs((int)(num*1000));
+		if(answer%10>=5) {
+			return sign*((int)((answer+10)/10))/100.0;
+		}else{
+			return sign*(answer/10)/100.0;
+		}
+		
+	}
+	
 }
