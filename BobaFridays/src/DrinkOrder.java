d6f7a85c5d4ec535139fdd7a51eb367fb25ebf4f
PostAPProject <50377770+PostAPProject@users.noreply.github.com>
2019-05-19 17:16:44 -0700

    Added overriding method for sweetness and ice

--- BobaFridays/src/DrinkOrder.java
+++ BobaFridays/src/DrinkOrder.java
@@ -1,25 +1,29 @@
 /*
- *@author Daric Zhou
- *@version 18 May 2019
+ * @author Daric Zhou, Jakin Chan, Jon Lee
+ * @version May 2019
+ * This class stores an order of a drink
  */
-
 import java.util.ArrayList;
 
 public class DrinkOrder extends T4Order {
 		private ArrayList<String> toppings = new ArrayList<String>();
+		private int sweetnessLevel, iceLevel;
 
-		public DrinkOrder(String o, ArrayList<String> tops, double price) {
+		public DrinkOrder(String o, ArrayList<String> tops, double price, int sweetness, int ice) {
 			super(o, price);
 			toppings = tops;
+			sweetnessLevel = sweetness;
+			iceLevel = ice;
 			for(@SuppressWarnings("unused") String topping : toppings) {
 				price += 0.50;
 			}
 		}
+		
 		public ArrayList<String> getToppings() {
 			return toppings;
 		}
-		public String toString() {
-			return getCustomerName() + ": " + getOrderName() + " with " + toppings + "\n Total cost: " + getPrice();
+		//overrides superclass method by including sweetness and ice
+		public String getOrderName() {
+			return getOrderName() + "and" + sweetnessLevel + "% sweetness, " + iceLevel +  "% ice";
 		}
-		
 	}
